3b1044ecfd2f551a956b9b95cab2ebd806ce5738
khazarkhorrami <k.khazar@gmail.com>
2021-11-18 13:41:12 +0200

    saving wavs added

--- annotation/yamnet/main.py
+++ annotation/yamnet/main.py
@@ -24,23 +24,23 @@ yamnet_settings = cfg.yamnet_settings
 from analysis import Analysis
 
 run_analysis = Analysis( audio_model,dataset, datadir, outputdir,split,  yamnet_settings )
-run_analysis()
+run_analysis.save_wav_clips()
 
-import json
-with open("/run/media/hxkhkh/b756dee3-de7e-4cdd-883b-ac95a8d00407/video/youcook2/annotations/youcookii_annotations_trainval.json", 'rb') as handle:
-    b = json.load(handle)
-database = b['database']
+# import json
+# with open("/run/media/hxkhkh/b756dee3-de7e-4cdd-883b-ac95a8d00407/video/youcook2/annotations/youcookii_annotations_trainval.json", 'rb') as handle:
+#     b = json.load(handle)
+# database = b['database']
 
-import pickle
-with open("/worktmp/khorrami/project_5/video/features/ouput/youcook2/ann-based/training_onsets", 'rb') as handle:
-    af = pickle.load(handle)
+# import pickle
+# with open("/worktmp/khorrami/project_5/video/features/ouput/youcook2/ann-based/training_onsets", 'rb') as handle:
+#     af = pickle.load(handle)
 
-import csv 
-with open("/run/media/hxkhkh/b756dee3-de7e-4cdd-883b-ac95a8d00407/video/youcook2/features/feat_csv/train_frame_feat_csv/101/17v08qtr8UM/0004") as handle:
-    vf = csv.reader(handle)
-    rows = []
-    for row in vf:
-        rows.append(row)
+# import csv 
+# with open("/run/media/hxkhkh/b756dee3-de7e-4cdd-883b-ac95a8d00407/video/youcook2/features/feat_csv/train_frame_feat_csv/101/17v08qtr8UM/0004") as handle:
+#     vf = csv.reader(handle)
+#     rows = []
+#     for row in vf:
+#         rows.append(row)
     
 
 
